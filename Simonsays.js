28d8cc480120f72e259c2b37165de0f52c5935ee
Magonz30 <manuelalonsogonzales@alumnat.iesdamiahuguet.net>
2025-04-06 22:20:37 +0200

    Juego terminado

--- Simonsays.js
+++ Simonsays.js
@@ -1,39 +1,70 @@
-const colors = ["rojo", "azul", "amarillo", "verde", "negro", "blanco", "naranja", "morado"];
-let serie = []
+const colors = ["rojo", "azul", "amarillo", "verde", "negro", "gris", "naranja", "morado"];
+let serie = [];
 let posicio;
+let entradaUsuario = []; //serie del usuario
+let esperandoInput = false; //determina que el usuario no pueda interactuar con los div (botones) al principio
 
+// Mostrar visualmente los colores
 async function mostrar_colors(temps){
- for (let i=0;i<serie.length;i++){
-  //TODO instruccions per mostrar el color. Per exemple
-
-  //Crida al mètode esperar
+ esperandoInput = false; //no deja al jugador interactuar con los div
+ for (let i = 0; i < serie.length; i++){
+  let color = serie[i];
+  let casilla = document.getElementById(color);
+  casilla.style.opacity = "0.3";  // efecto de parpadeo
   await esperar(temps);
+  casilla.style.opacity = "1000";
+  await esperar(200);
   console.log(serie[i]);
  }
+ entradaUsuario = [];//determina que nuestra serie personal empiece desde cero asi tenemos que hacer todo otra vez
+ esperandoInput = true; // despues de hacer la serie nos deja jugar
 }
+
+// Dice el tiempo que hay que esperar en la serie
 function esperar(milliseconds){
  return new Promise(resolve => {
   setTimeout(resolve, milliseconds);
  });
 }
 
-
+// Añade un color del array aleatorio a la serie
 function random () {
  posicio = Math.floor(Math.random() * 8);
- console.log(posicio);
  serie.push(colors[posicio]);
  console.log(serie);
 }
 
-function comprova (color){
- random()
+//Verifica la jugada del jugador
+function comprovaColor(color){
+ if (!esperandoInput) return; //no nos deja interactuar mientras hacemos la funcion
 
- if (color === "azul"){
-  document.getElementById('azul').innerHTML = '<h2>' + 'Azul' + '</h2>';
+
+ entradaUsuario.push(color); // es lo que seleccionamos
+ let index = entradaUsuario.length - 1; //-1 pq asi lo lee el array
+
+ //comprueba que añamos puesto lo mismo que la serie si no es asi nos salta una alerta de error y reinicia todx
+
+ if (entradaUsuario[index] !== serie[index]) {
+  alert("¡Error! Empieza de nuevo.");
+  serie = [];
+  entradaUsuario = [];
+  esperandoInput = false;
+  return;
  }
- if (color === "rojo"){
-  document.getElementById('rojo').innerHTML = '<h2>' + 'Rojo' + '</h2>';
+
+ //comprueba que añamos puesto lo mismo que la serie
+ if (entradaUsuario.length === serie.length) {
+  esperandoInput = false;
+  setTimeout(() => {
+   comprova();  // ir al siguiente nivel
+  }, 1000);
  }
+}
+
+//Añade un color aleatorio y muestra la serie
+function comprova (){
+ random();
+ mostrar_colors(100);
 }
 
 
